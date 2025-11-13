bd87f4553b96326a999ceaaade5aacec5423bd4f
Miguel Rodrigues <miguel.rodriguesmorais.cruz@gmail.com>
2025-11-12 23:03:43 -0300

    commit

---
+++ JS/introducao.js
@@ -0,0 +1,28 @@
+//alert('Hello World !!!')
+
+console.log('Hello World !!!')
+
+/**Tipos Primitivos
+    String;
+    Number;
+    Boolean;
+    Null;
+    Undefined;
+ */
+
+var nome;
+var valorCurso = 550.42;
+nome = 'impacta';
+
+console.log(valorCurso / 10.00);
+console.log(valorCurso + 10.00);
+console.log(valorCurso - 10.00);
+console.log(valorCurso * 10.00);
+
+var idade = 10;
+
+var soma;
+
+soma = ++idade;
+
+console.log(soma);
