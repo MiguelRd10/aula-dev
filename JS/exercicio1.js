5348001965f0033036d53af3e4c91cf1bd0521c0
Miguel Rodrigues <miguel.rodriguesmorais.cruz@gmail.com>
2025-11-14 22:25:44 -0300

    verifica produto

--- JS/exercicio1.js
+++ JS/exercicio1.js
@@ -1,51 +1,104 @@
+
+(function (calc) {
+    let interromper = true;
+    while(interromper) {
+        const imposto = 0.2;
+        const produtos = [
+            ['Sabão', 2.00], // cod 1
+            ['Leite', 5.00], // cod 2
+            ['Açucar',5.50], // cod 3
+        ];
+        const dados = [];
+        const mensagens = [
+            'codigo do produto',
+            'digite a quantidade',
+        ];
+
+
+        for(let i = 0; i < mensagens.length; i++) {
+            let dado = prompt(mensagens[i]) 
+            dados.push(dado);
+        }
+
+        const cod = dados[0];
+        const produtoSelecionado = produtos[cod - 1]
+
+
+        if (!produtoSelecionado) {
+            alert('Produto não existe');
+        }
+
+         const totalCompra = calc(dados[1], produtoSelecionado[1], imposto);
+
+        console.log(`Produto:  ${produtoSelecionado[0]}`);
+        console.log(`Valor Unitario: ${produtoSelecionado[1]}`);
+        console.log(`Total da compra: ${totalCompra}`);
+        console.log(`Imposto cobrado: 20%`);
+
+        interromper = confirm('Deseja continuar');
+    }
+
+
+})(calculoTotalCompra)
+
+
+
+function calculoTotalCompra(quantidade, valorProduto, imposto = null) {
+    const totalCompraSemImposto = quantidade * valorProduto;
+    if (imposto) {
+        const total = totalCompraSemImposto + totalCompraSemImposto * imposto;
+        return total;
+    }
+    return totalCompraSemImposto;
+}
+
+
+
+
+
+
+// let valor = 50;
+// console.log('Imprime 1: ', valor);
+
+// function soma(v,x) {
+//     let incremento = 1;
+//     valor = v + incremento
+// }
+
+// console.log('Imprime 2: ', valor);
+
+// function subtrair() {
+//     soma(100, 10);
+// }
+
+// subtrair()
+
+
+
 /*
+ Desenvolva um sistema de compra onde o usuario digitará:
+    - nome do produto 
+    - Quantidade de produto que comprou
+    - valor de uma unidade
+    - nome do cliente
 
-Desenvolva um sistema de compra onde o usuario digitará:
- - Nome do produto
- - Quantidade de progduto que comprou
- - Valor de uma unidade
- - nome do cliente 
+    Seu programa deverá calcular o total da compra  e imprimir as informações da compra:
 
+    Exemplo de saida
+    ###############################
 
- Seu programa deverá calcular o total de compra e imprimir as informações da compa:
+    Produto: xxxxx
+    Valor Unitario: xxxx
+    Total da compra (com imposto): xxxxx
+    Imposto cobrado: 20%
 
+    -------------------------------------
+    Obrigado e volte sempre.
 
- Emplo de saída
- -----------------------------------------------
+    var nomeCliente = prompt('digite seu nome');
 
- Nome do cliente: XXXXXXXX
- Produto: XXXXX
- Total de compra: XXXXXXX
+    Imposto sobre total da compra
+    Caucule um imposta de 20% sobre o total da compra
 
- ------------------------------------------------
- Obrigado e volte sempre
-
- 
+    Formuala = totalCompra + totalCompra * 0.2
 */
-var nomeProduto 
-var quantidadeProduto
-var valorProduto
-var nomeCliente
-
-if(nomeProduto = ''){
-    nomeProduto = prompt('digite o nome do produto')
-} else if(quantidadeProduto = ''){
-    quantidadeProduto = prompt('digite a quantidade de produtos')
-} else if(valorProduto = ''){
-    valorProduto = prompt('digite o valor do produto')
-}else if (nomeCliente = ''){
-    nomeCliente = prompt('digite seu Nome')
-}
-
-
-console.log('Nome do Cliente:', nomeCliente);
-console.log('Produto:', nomeProduto);
-
-var totalCompra = quantidadeProduto * valorProduto;
-
-console.log('Total de compra:', totalCompra)
-
-if(nomeCliente != '' && nomeProduto != '' ){
-    console.log('Obrigado e Volte Sempre!')
-}
-
